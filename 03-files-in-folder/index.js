770e2338507a9715f6d1789cfe66fd7ad7be9fa8
L1senochek <lisenochek96@gmail.com>
2023-05-07 00:33:26 +0300

    feat: add 03-files-in-folder task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const pathFolder = path.join(__dirname, 'secret-folder');
+
+fs.readdir(pathFolder, { withFileTypes: true }, (error, files) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  files.forEach((file) => {
+    if (file.isFile()) {
+      const filePath = path.join(pathFolder, file.name);
+      const fileSize = fs.statSync(filePath).size;
+      console.log(`${file.name} - ${path.extname(file.name).slice(1)} - ${(fileSize/1024).toFixed(2)} kb`);
+    } else {
+      return;
+    }
+  });
+});
